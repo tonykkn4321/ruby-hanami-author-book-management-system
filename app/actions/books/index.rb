847a6b1cafb0e03ccdfc3f8ef75380be77e7a906
tonykkn4321 <tonykkn4321@gmail.com>
2025-08-31 22:26:03 +0800

    Create index.rb

---
+++ app/actions/books/index.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Tx
+  module Actions
+    module Books
+      class Index < Tx::Action
+        def handle(request, response)
+        end
+      end
+    end
+  end
+end
