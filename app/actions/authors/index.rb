13ea78c4554a7f725021b1cfe8f57309d6dc8674
tonykkn4321 <tonykkn4321@gmail.com>
2025-08-31 23:48:52 +0800

    Update index.rb

--- app/actions/authors/index.rb
+++ app/actions/authors/index.rb
@@ -1,10 +1,14 @@
-# frozen_string_literal: true
-
 module Tx
   module Actions
     module Authors
-      class Index < Tx::Action
+      class Index < Hanami::Action
         def handle(request, response)
+          authors = AuthorRepository.new.all
+
+          response.render(
+            view: Tx::Views::Authors::Index.new,
+            context: { authors: authors }
+          )
         end
       end
     end
