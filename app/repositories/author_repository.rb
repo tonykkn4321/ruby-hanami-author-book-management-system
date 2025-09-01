ad580505a9a4b0c7e08d66a16e80ff171903994f
Tony Kwong <tonykkn4321@gmail.com>
2025-09-01 12:48:57 +0800

    Modified repository files

--- app/repositories/author_repository.rb
+++ app/repositories/author_repository.rb
@@ -1,4 +1,13 @@
-class AuthorRepository < Hanami::Repository
+module Tx::Repositories
+  class AuthorRepository < ROM::Repository[:authors]
+    commands :create, update: :by_pk, delete: :by_pk
+
+    def all
+      authors.to_a
+    end
+
+    def find(id)
+      authors.by_pk(id).one
+    end
+  end
 end
-
-
--- app/repositories/book_repository.rb
+++ app/repositories/book_repository.rb
@@ -1,2 +1,13 @@
-class BookRepository < Hanami::Repository
+module Tx::Repositories
+  class BookRepository < ROM::Repository[:authors]
+    commands :create, update: :by_pk, delete: :by_pk
+
+    def all
+      authors.to_a
+    end
+
+    def find(id)
+      authors.by_pk(id).one
+    end
+  end
 end
