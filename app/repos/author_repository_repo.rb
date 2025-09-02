3096c6737114f57d15e88837bc512e1f134890b6
tonykkn4321 <tonykkn4321@gmail.com>
2025-09-02 22:40:00 +0800

    Update author_repository_repo.rb

--- app/repos/author_repository_repo.rb
+++ app/repos/author_repository_repo.rb
@@ -3,10 +3,7 @@
 module RubyHanamiAuthorBookManagementSystem
   module Repos
     class AuthorRepositoryRepo < RubyHanamiAuthorBookManagementSystem::DB::Repo
-      # Example: Find authors by last name
-      def find_by_last_name(last_name)
-        where(last_name: last_name).to_a
-      end
+      
     end
   end
 end
