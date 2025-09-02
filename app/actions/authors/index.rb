dacd00acdae450e4232eca39100ee06c17e5cd19
tonykkn4321 <tonykkn4321@gmail.com>
2025-09-02 22:44:03 +0800

    Update index.rb

--- app/actions/authors/index.rb
+++ app/actions/authors/index.rb
@@ -5,8 +5,11 @@ module RubyHanamiAuthorBookManagementSystem
     module Authors
       class Index < RubyHanamiAuthorBookManagementSystem::Action
         def handle(request, response)
-          @authors = AuthorRepository.new.all
-          response.body = @authors.to_json
+
+          authors = AuthorRepository.new.all
+          res.body = authors.map(&:to_h).to_json
+          #@authors = AuthorRepository.new.all
+          #response.body = @authors.to_json
         end
       end
     end
