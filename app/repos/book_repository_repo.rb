dc6d53ba622a309baff5fdf078a7ff704e173a10
tonykkn4321 <tonykkn4321@gmail.com>
2025-09-02 22:56:12 +0800

    Update book_repository_repo.rb

--- app/repos/book_repository_repo.rb
+++ app/repos/book_repository_repo.rb
@@ -3,15 +3,7 @@
 module RubyHanamiAuthorBookManagementSystem
   module Repos
     class BookRepositoryRepo < RubyHanamiAuthorBookManagementSystem::DB::Repo
-      # Example: Find books by a specific author
-      def find_by_author(author_id)
-        where(author_id: author_id).to_a
-      end
-
-      # Example: Find all books published in a specific year
-      def find_by_year(year)
-        where(year: year).to_a
-      end
+      
     end
   end
 end
