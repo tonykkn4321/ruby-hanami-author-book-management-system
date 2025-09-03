d7b1b4dac9c8d74a0d78aba42a6e0687d45676ec
tonykkn4321 <tonykkn4321@gmail.com>
2025-09-03 14:21:34 +0800

    Update index.rb

--- app/actions/authors/index.rb
+++ app/actions/authors/index.rb
@@ -7,8 +7,7 @@ module RubyHanamiAuthorBookManagementSystem
     module Authors
       class Index < RubyHanamiAuthorBookManagementSystem::Action
         def handle(request, response)
-          # Fully qualify the repository to avoid NameError
-          authors_repository = RubyHanamiAuthorBookManagementSystem::App::Repo::AuthorsRepository.new(rom)
+          authors_repository = AuthorsRepository.new(rom)
           authors = authors_repository.all
           response.json(authors)
         end
