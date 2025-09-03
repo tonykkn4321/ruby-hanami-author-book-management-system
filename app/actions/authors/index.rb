39003a693a8c16110d8560636138e1bb35a1bf8d
tonykkn4321 <tonykkn4321@gmail.com>
2025-09-03 13:23:59 +0800

    Update index.rb

--- app/actions/authors/index.rb
+++ app/actions/authors/index.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-require_relative '../../repos/authors_repository' # Adjust path as needed
+require_relative '../../repos/authors_repository' # Correct path to the repository
 
 module RubyHanamiAuthorBookManagementSystem
   module Actions
     module Authors
       class Index < RubyHanamiAuthorBookManagementSystem::Action
         def handle(request, response)
-          authors = AuthorsRepository.new.all
+          # Instantiate the repository with the rom instance
+          authors_repository = AuthorsRepository.new(rom)
+          authors = authors_repository.all
           response.json(authors)
         end
       end
     end
   end
 end
-
-
