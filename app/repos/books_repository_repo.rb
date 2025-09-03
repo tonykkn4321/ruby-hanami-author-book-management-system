e77e544130f076d15bc5693a9c495ecacbae0af7
tonykkn4321 <tonykkn4321@gmail.com>
2025-09-03 11:15:53 +0800

    Update books_repository_repo.rb

--- app/repos/books_repository_repo.rb
+++ app/repos/books_repository_repo.rb
@@ -3,6 +3,19 @@
 module RubyHanamiAuthorBookManagementSystem
   module Repos
     class BooksRepositoryRepo < RubyHanamiAuthorBookManagementSystem::DB::Repo
+     def initialize(rom)
+      @rom = rom
+     end
+
+     # Retrieve all books
+     def all
+      @rom.relation(:books).to_a # Fetch all books as an array
+     end
+
+     # Create a new book
+     def create(book_params)
+      @rom.relation(:books).insert(book_params) # Insert new book
+     end
     end
   end
 end
