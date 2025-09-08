31134bea1ce86d985449d007f957a957b7e08dd2
Tony Kwong <tonykkn4321@gmail.com>
2025-09-08 10:21:56 +0800

    Modified authors.actions.index

--- app/actions/authors/index.rb
+++ app/actions/authors/index.rb
@@ -47,8 +47,7 @@ module RubyHanamiAuthorBookManagementSystem
           {
             id: author.id,
             first_name: author.first_name,
-            last_name: author.last_name,
-            created_at: author.created_at
+            last_name: author.last_name
           }
         end
       end
